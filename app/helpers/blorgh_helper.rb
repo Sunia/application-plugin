bfbf3e4b712b3f8eff51f936d0e22e3d3632739c
Sunia kalra <skalra17@yahoo.in>
2015-04-07 17:19:39 +0530

    modified plugin helper.

--- app/helpers/blorgh_helper.rb
+++ app/helpers/blorgh_helper.rb
@@ -2,26 +2,13 @@
 module BlorghHelper
 
   def user_signed_in?
-    warden.authenticate?(scope: mapping)
+    puts 'helper method of plugin is called.'
   end
 
   def current_user
-    mappings.unshift mappings.delete(favourite.to_sym) if favourite
-    mappings.each do |mapping|
-      current = warden.authenticate(scope: mapping)
-      return current if current
-    end
-    nil
   end
 
   def authenticate_user
-    unless user_signed_in?
-      mappings.unshift mappings.delete(favourite.to_sym) if favourite
-      mappings.each do |mapping|
-        opts[:scope] = mapping
-        warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
-      end
-    end
   end
 
 end
